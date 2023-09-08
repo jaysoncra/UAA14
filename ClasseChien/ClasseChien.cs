cfdc32b6c34e4c48555027043c32914d7debb4cb
jyaso <220484@site.asty-moulin.be>
2023-09-08 13:13:00 +0200

    first commit

---
+++ ClasseChien/ClasseChien.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseChien
+{
+    internal class ClasseChien
+    {
+        private string _nom;
+        private string _race;
+        private bool _purRace;
+        private string _provenance;
+        private bool _genre;
+        private uint _age;
+        private string _couleur;
+        private bool _albinos;
+        private bool _melanisme;
+        private string _particularitePhysique;
+        private bool _enOrdreDeVaccin;
+        private bool _puce;
+        private bool _sterilise;
+        private bool _dresse;
+
+
+        public ClasseChien(string nom, string race, bool purRace, string provenance, bool genre, uint age, string couleur, bool albinos, bool melanisme, string particularitePhysique, bool enOrdreDeVaccin, bool puce, bool sterilise, bool dresse)
+        {
+            _nom = nom;
+            _race = race;
+            _purRace = purRace;
+            _provenance = provenance;
+            _genre = genre;
+            _age = age;
+            _couleur = couleur;
+            _albinos = albinos;
+            _melanisme = melanisme;
+            _particularitePhysique = particularitePhysique;
+            _enOrdreDeVaccin = enOrdreDeVaccin;
+            _puce = puce;
+            _sterilise = sterilise;
+            _dresse = dresse;
+        }
+
+        public string AfficheCaracteristiques()
+        {
+            string chaine = "Nom : " + _nom + " Race - " + _race + " Pur race - " + _purRace + " Provenance - " + _provenance + " Genre - " + _genre + " Åge - " + _age + " Couleur - " + _couleur + " Albinos - " + _albinos + " Mélanisme - " + _melanisme + " Particularité physique - " + _particularitePhysique + " En ordre de vaccin - " + _enOrdreDeVaccin + " Puce - " + _puce + " Stérilise - " + _sterilise + " Dressé - " + _dresse;
+            return chaine;
+        }
+
+
+    }
+}
---
+++ ClasseChien/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Chien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Liste des chiens");
+            ClasseChien chien1 = new ClasseChien("Happy", "Doberman", true, "Bosnie", "Male", "Noir et Feu", false, false, "Oreilles et queue coupées", true, true, false, true);
+        }
+    }
+}
