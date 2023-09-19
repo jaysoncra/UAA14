fa231c324d855c181fde5bb139ecf0e883d9b2fb
jyaso <220484@site.asty-moulin.be>
2023-09-19 14:48:03 +0200

    third push

---
+++ ACT_2_POO_EXERCICES_Simples/Exercice 1 = Le cercle/LeCercle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_1___Le_cercle
+{
+    class LeCercle
+    {
+        private double _rayon;
+
+        public LeCercle(double rayon)
+        {
+            _rayon = rayon;
+        }
+
+        public double Rayon
+        {
+            get { return _rayon; }
+            set { _rayon = value; }
+        }
+
+       public double CalculeAire()
+
+    }
+}
